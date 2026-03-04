a31ecc96e6bfc53fe4defaddf76a6434a9c7bc0b
SaiBhargav05 <saibhargav052000@gmail.com>
2026-03-04 17:11:36 -0500

    encapsulation, getter, setter, static method covered

--- oops1.py
+++ oops1.py
@@ -2,11 +2,12 @@
 class Employee:
     # special function/dunder method - constructor
     def __init__(self):
-        print("Started executing the attributes/ data")
+        print(id(self))
+        #print("Started executing the attributes/ data")
         self.id =123
         self.salary=25000
         self.designation="SDE"
-        print("attributes/data have been initiated")
+        #print("attributes/data have been initiated")
 
     def travel(self, destination):
         print("This travel method was called manually")
@@ -16,6 +17,12 @@ class Employee:
 
 # creating an object of the class Employee     
 sam = Employee()
+sam.name = "sam kumar" ## we can add attributes even outside the constructor
+print(sam.name) ## we can access the attributes even outside the constructor
+#print(id(sam)) ## what we observe here is that the id of the object sam is same as the id printed in the constructor which means that the constructor is automatically called when we create an object of the class and it initializes the attributes of the class for that object.
+
+# hero = Employee() 
+# print(id(hero)) 
 # printing attributes
 #print(sam.id)
 ## calling a method
--- rough.py
+++ rough.py
@@ -9,3 +9,30 @@
 
 from oops_project import chatbook
 user1 = chatbook()
+print(user1.id)
+
+chatbook.set_id(100)
+
+user2 = chatbook()
+print(user2.id)
+
+# getter and setter methods are shown in the below code snippet
+# print(user1.get_name())
+# user1.set_name("Hero")
+# print(user1.get_name())
+
+
+# function vs method is shown in the below code snippet
+
+# lst = [1,2,3]
+
+
+# # function 
+# a1 = len(lst)
+# print(a1)   
+
+# #method
+# user1 = chatbook()
+# user1.send_message()
+
+
